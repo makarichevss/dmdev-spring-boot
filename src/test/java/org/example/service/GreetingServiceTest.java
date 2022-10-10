863667c9092dece4a97cf0eb41898d10607b81c2
Sergey Makarichev <harleymack@mail.ru>
2022-10-10 17:00:57 +0300

    first Spring Boot test

---
+++ src/test/java/org/example/service/GreetingServiceTest.java
@@ -0,0 +1,32 @@
+package org.example.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class GreetingServiceTest {
+
+    @Autowired
+    private GreetingService greetingService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testGreeting() {
+        String expectedMessage = "Hello world!2";
+        assertEquals(expectedMessage, greetingService.greet());
+    }
+}
